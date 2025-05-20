d6d69e1adbd65e6086c7351c9e9bbbaa1d0ffaf3
Priyanoojps <priyanoozsaikia@gmail.com>
2025-05-20 12:39:25 +0530

    Add files via upload

---
+++ all.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+import matplotlib.image as mpimg
+import math
+import plotly.express as px
+import plotly.graph_objects as go
+
+from matplotlib.offsetbox import (TextArea, DrawingArea, OffsetImage,
+                                  AnnotationBbox)
+from plotly.colors import n_colors
+from plotly.subplots import make_subplots
+from IPython.display import Image
+from colorama import Fore, Back, Style
+y_ = Fore.YELLOW
+r_ = Fore.RED
+g_ = Fore.GREEN
+b_ = Fore.BLUE
+m_ = Fore.MAGENTA
+sr_ = Style.RESET_ALL
